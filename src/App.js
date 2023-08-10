38601cc19d9b7b247a147178919af036e3d50027
dev-RowanSong <hyeho331@gmail.com>
2023-08-10 18:45:28 +0900

    [Fix] Change App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
-import { Mobile, Tablet, PC } from './layout/ResponsiveView';
+import { Mobile, PC } from './layout/ResponsiveView';
+import Header from './layout/Header';
 
 function App() {
     return (
         <div className="App">
-            <Mobile></Mobile>
-            <PC size="768"></PC>
+            <Mobile>
+                <Header device="mobile"></Header>
+            </Mobile>
+
+            <PC size="768">
+                <Header></Header>
+            </PC>
         </div>
     );
 }
