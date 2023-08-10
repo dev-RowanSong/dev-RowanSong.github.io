00d5e7eb8e7d7360c6e6de7eaabc1089e5e91665
dev-RowanSong <hyeho331@gmail.com>
2023-08-10 18:43:58 +0900

    [Add] add ResponsiveView Component

--- src/layout/ResponsiveView.js
+++ src/layout/ResponsiveView.js
@@ -4,7 +4,7 @@ const MOBILE_REF_SIZE = 768;
 const TABLET_REF_SIZE = 1024;
 
 /**
- * 화면 사이즈에 따른 레이아웃 반환
+ * 모바일 뷰 (default: ~ 767px)
  */
 export const Mobile = ({children, size = MOBILE_REF_SIZE}) => {
     const isMobile = useMediaQuery({
@@ -12,12 +12,20 @@ export const Mobile = ({children, size = MOBILE_REF_SIZE}) => {
     });
     return <>{isMobile && children}</>;
 };
+
+/**
+ * 테블릿 뷰 (default: 768px ~ 1023px)
+ */
 export const Tablet = ({children, size = [MOBILE_REF_SIZE, TABLET_REF_SIZE]}) => {
     const isTablet = useMediaQuery({
         query: `(min-width:${size[0]}px) and (max-width:${size[1] - 1}px)`
     });
     return <>{isTablet && children}</>;
 };
+
+/**
+ * PC 뷰 (default: 1024px ~)
+ */
 export const PC = ({children, size = TABLET_REF_SIZE}) => {
     const isPC = useMediaQuery({
         query: `(min-width:${size}px)`
